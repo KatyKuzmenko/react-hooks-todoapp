1ec325320ca696078880c18b6ca34c2b91eae6cb
Katy Kuzmenko <rina.punina@gmail.com>
2021-12-24 11:28:19 +0200

    odd request methods removed

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,6 @@
 const BASE_URL = 'http://localhost:3001'
 
-const request = (url, options) => {
-  return fetch(`${BASE_URL}${url}`, options).then((response) => {
-    if (!response.ok) {
-      return `${response.status} - ${response.statusText}`
-    }
-
-    return response.json()
-  })
-}
 export async function callApi(url, options) {
   const response = await fetch(`${BASE_URL}${url}`, options)
   return response.json()
 }
-
-export const getTodos = () => request('/todos')
-
-export const getTodo = (todoId) => request(`/todos/${todoId}`)
