7177f33775b69001c447b146dfdcc3e2c25b734c
Katy Kuzmenko <rina.punina@gmail.com>
2021-12-27 12:05:10 +0200

    loading actions added

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux'
-import todosReducer from './reducer'
+import todosReducer from './todosReducer'
 import { appReducer } from './appReducer'
 
 export const rootReducer = combineReducers({
