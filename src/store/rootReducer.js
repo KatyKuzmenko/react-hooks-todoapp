48582bf706ec9590ce75f0b9f447fee20ecb8695
Katy Kuzmenko <rina.punina@gmail.com>
2021-12-28 18:43:37 +0200

    typescript added

--- src/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import todosReducer from './todosReducer'
-import { appReducer } from './appReducer'
-
-export const rootReducer = combineReducers({
-  todos: todosReducer,
-  app: appReducer,
-})
